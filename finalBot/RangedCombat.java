50b8af8fb2fa8430c6c0091a73b1cec98f6992bc
Jonah Casebeer <jonah.casebeer@gmail.com>
2017-01-10 12:47:23 -0500

    framework for shooting and moving micro

---
+++ finalBot/RangedCombat.java
@@ -0,0 +1,113 @@
+package team008.finalBot;
+import battlecode.common.*;
+
+/**
+ * Created by jmac on 1/10/17.
+ */
+public class RangedCombat extends Bot {
+    private static final String MOVE_FIRST = "move first";
+    private static final String SHOOT_FIRST = "shoot first";
+    private static final String SINGLE_SHOT = "single shot";
+    private static final String TRIAD_SHOT = "triad shot";
+    private static final String PENTAD_SHOT = "pentad shot";
+
+    //forgive me for I have sinned
+    private static String shotType = "";
+
+
+    public static void execute() throws GameActionException{
+
+        String firstAction = determineFirstAction();
+        MapLocation destination;
+        RobotInfo target;
+        RobotInfo[] robotsInSight = rc.senseNearbyRobots();
+
+            if(firstAction == MOVE_FIRST){
+                //move to destination
+                destination = chooseMove();
+
+                //shoot target
+                target = chooseTargetAndShotType(robotsInSight);
+                shootIfWorth(target, shotType);
+            } else {
+                //move to destination
+                destination = chooseMove();
+
+                //shoot target
+                target  = chooseTargetAndShotType(robotsInSight);
+                shootIfWorth(target, shotType);
+            }
+
+    }
+
+    private static String determineFirstAction() throws GameActionException{
+        //check if our best shot is in the direction we’re moving
+        return MOVE_FIRST;
+    }
+
+    private static MapLocation chooseMove() throws GameActionException{
+        //evade bullets
+        //decide whether to engage
+            //can we win 1v1?
+            //will we have backup?
+        //move to assist someone else
+        //move to put us in best spot
+        return null;
+    }
+
+    ///////////////////// Shooting and Target Micro/////////////////////
+    private static RobotInfo chooseTargetAndShotType(RobotInfo[] robotsInSight) throws GameActionException{
+        int score;
+        int bestScore = 999999;
+        RobotInfo bestTarget = null;
+        for(RobotInfo robot: robotsInSight){
+            //add other factors for choosing best bot
+            //value based on num nearby bots including trees
+            score = (robot.getType().maxHealth - (int)robot.getHealth());
+
+            if(score < bestScore){
+                bestScore = score;
+                bestTarget = robot;
+            }
+        }
+
+        shotType = calculateShotType(bestTarget,bestScore);
+        return bestTarget;
+    }
+
+    private static String calculateShotType(RobotInfo target, int score) throws GameActionException{
+        //come up with some sort of formula for choosing the kind of shot
+        return SINGLE_SHOT;
+    }
+
+    private static void shootIfWorth(RobotInfo target, String shotType) throws GameActionException{
+        switch (shotType){
+            case SINGLE_SHOT: shootSingleShot(target);
+            case TRIAD_SHOT: shootTriadShot(target);
+            case PENTAD_SHOT: shootPentadShot(target);
+                default: //do nothing, it isn't worth shooting.
+        }
+    }
+
+
+
+    ///////////////////// These Might Belong in Util/////////////////////
+    private static void shootSingleShot(RobotInfo target) throws GameActionException{
+        if (rc.canFireSingleShot()) {
+            rc.fireSingleShot(rc.getLocation().directionTo(target.location));
+        }
+
+    }
+    private static void shootTriadShot(RobotInfo target) throws GameActionException{
+        if (rc.canFireTriadShot()) {
+            rc.fireSingleShot(rc.getLocation().directionTo(target.location));
+        }
+
+    }
+    private static void shootPentadShot(RobotInfo target) throws GameActionException{
+        if (rc.canFirePentadShot()) {
+            rc.fireSingleShot(rc.getLocation().directionTo(target.location));
+        }
+
+    }
+}
