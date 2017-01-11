58d83e08049ed3ede1d8b46d9d8eb81612741fec
Jonah Casebeer <jonah.casebeer@gmail.com>
2017-01-11 12:28:50 -0500

    having tanks do ranged combat

--- finalBot/Tank.java
+++ finalBot/Tank.java
@@ -7,8 +7,30 @@ public class Tank extends Bot {
 	public Tank(RobotController r){
 		super(r);
 	}
-	
+
 	public void takeTurn(TreeInfo[] nearbyNeutralTrees) throws Exception{
-        return;
+		RobotInfo[] enemies = rc.senseNearbyRobots(-1,enemy);
+		if(enemies.length > 0){
+			if(rc.getRoundNum() % 25 == 0){
+				Util.notifyFriendsOfEnemies(enemies);
+			}
+			RangedCombat.execute();
+			return;
+		}
+		if(target == null){
+			assignNewTarget();
+		}
+		else if (target != null && rc.getLocation().distanceTo(target) < 2 && enemies.length == 0){
+			Messaging.removeEnemyArmyLocation(target);
+			Messaging.removeEnemyUnitLocation(target);
+			target = null;
+			assignNewTarget();
+		}
+		if(target != null){
+			goTo(target);
+		}
+		else{
+			tryMoveDirection(here.directionTo(Util.rc.getInitialArchonLocations(enemy)[0]));
+		}
 	}
 }
