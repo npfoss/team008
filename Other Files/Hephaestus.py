2fa47e8b686811f1aa3a49dfd2b2183858e48e61
Nate Foss <npfoss@gmail.com>
2017-01-24 23:32:29 -0500

    revamp hephaestus and get it to work on windows

--- Other Files/Hephaestus.py
+++ Other Files/Hephaestus.py
@@ -1,32 +1,65 @@
-import commands
+'''
+## Hephaestus, a system for mass testing your battlecode bot!
+
+In order to use Hephaestus you need python 3.x and Gradle. To run Hephaestus simply navigate to the battlecodeScaffold2017 root directory and run the following command. You may need to specify the python version if you havent set 2.x as your default.
+
+python Hephaestus.py <pkg of team a> <pkg of team b>
+
+Hephaestus will then play team a and team b against eachother on all the maps in both positions. It should return a result like the following.
+
+Team A won on shrine
+Team A won on Barrier
+Team A won on DenseForest
+Team A won on Enclosure
+Team A won on Hurdle
+Team A won on SparseForest
+Team A won on shrine
+Team A won on Barrier
+Team A won on DenseForest
+Team A won on Enclosure
+Team A won on Hurdle
+Team A won on SparseForest
+Team A won 12 games -- Team B won 0
+
+When the starting positions are flipped the output will keep the original names.
+
+Enjoy!
+'''
+import subprocess
 import sys
+
+def getMapNames():
+	infile = open('maps.txt', 'r') # r for read
+	names = [line.strip() for line in infile]
+	infile.close()
+	return names
+
+def runMatches(teamA, teamB, stats, maps, flipTeams=False):
+	for mapName in maps:
+		command = ['gradle', 'run', '-PteamA='+teamA, '-PteamB='+teamB, '-Pmaps='+mapName]
+		#print(command)
+		result = subprocess.check_output(command, shell=True).decode("utf-8")
+		winningTeam = result[result.find(') wins')-1]
+		if (flipTeams):
+			winningTeam  = ('A','B')[winningTeam == 'A']
+		stats[winningTeam]+=1
+		
+		print ('Team ' + winningTeam + ' won on '+mapName)
+
 if __name__ == '__main__':
 
-	mapNames = ['shrine','Barrier','DenseForest','Enclosure','Hurdle','SparseForest']
+	mapNames = getMapNames()
+	print("running on maps: ", mapNames)
 	teamA = sys.argv[1]
 	teamB = sys.argv[2]
 	gameStats = {'A': 0, 'B': 0} #index zero for team A index one for Team B
 	
 	#Play all the maps
-	for mapName in mapNames:
-		command = 'gradle run -PteamA='+teamA+' -PteamB='+teamB+' -Pmaps='+mapName
-		result = commands.getoutput(command)
-		#print result
-		winIndex = result.find('wins')
-		winningTeam = result[winIndex-3:winIndex-2]
-		gameStats[winningTeam]+=1
-		
-		print ('Team ' + winningTeam + ' won on '+mapName)
+	runMatches(teamA, teamB, gameStats, mapNames)
+
 	#Play all maps with reverse spots
-	for mapName in mapNames:
-		command = 'gradle run -PteamA='+teamB+' -PteamB='+teamA+' -Pmaps='+mapName
-		result = commands.getoutput(command)
-		#print result
-		winIndex = result.find('wins')
-		winningTeam = result[winIndex-3:winIndex-2]
-		winningTeam  = ('A','B')[winningTeam == 'A']
-		gameStats[winningTeam]+=1
-		print ('Team ' + winningTeam + ' won on '+mapName+' with positions flipped') 
+	print("flipping sides... (but not names: %s is still called team A but plays as blue)"%teamA)
+	runMatches(teamB, teamA, gameStats, mapNames, True)
 	
 	#Print the final results
 	print('Team A won ' + str(gameStats['A']) +' games -- Team B won ' + str(gameStats['B']) )
